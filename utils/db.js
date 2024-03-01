f67c2b524c95994e960abb87f5a5d76bd7ad9f57
Naveen <imnaveenbharath@gmail.com>
2024-03-01 15:06:01 +0530

    batman

---
+++ utils/db.js
@@ -0,0 +1,12 @@
+import { createConnection } from 'mysql2';
+
+function sql( DB_NAME=process.env.DB_NAME ) {
+  return createConnection({
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    database : DB_NAME,
+    password : process.env.DB_PASSWORD
+  });
+}
+
+export default sql;
